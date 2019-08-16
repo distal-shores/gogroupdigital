480351cc7f85fec745f0591f534de59d02f8665b
distal-shores <samcoll@gmail.com>
2019-08-16 12:14:29 -0700

    Add plugins to repo.

---
+++ wp-content/plugins/theme-plugin/taxonomies/taxonomy-privilege_level.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * CUSTOM TAXONOMIES: Template.
+ *
+ * To use, copy and rename file to taxonomy-TAX.php. Replace all instances of
+ * TAX with the desired slug of your taxonomy. All taxonomy-*.php files will be
+ * automatically included.
+ *
+ * For full documentation on taxonomies and all arguments and labels available,
+ * see http://codex.wordpress.org/Function_Reference/register_taxonomy.
+ */
+
+
+add_action('init', function(){
+
+    /*
+     * These variables define the taxonomy slug, labels, and the post type to be used with.
+     * These must be set.
+     */
+
+    $slug = 'privilege_level';
+    $singular = 'Privilege Level';
+    $plural = 'Privilege Levels';
+    $type = 'blog_post';
+
+    $taxonomy = array(
+
+        // The slug for your custom taxonomy
+        'name' => $slug,
+
+        // Which post types can use this taxonomy?
+        'post_types' => array($type), // <--- CHANGE ME TOO
+
+        // Configuration for the taxonomy.
+        'args' => array(
+            "hierarchical" => true,
+            "rewrite" => true,
+            "public" => true,
+            'query_var' => true,
+            'show_admin_column' => true,
+
+            // Labels are highly configurable
+            // Full into: http://codex.wordpress.org/Function_Reference/register_taxonomy#Arguments
+            "label" => $plural,
+            "singular_label" => $singular
+            )
+        );
+
+    register_taxonomy($taxonomy['name'], $taxonomy['post_types'], $taxonomy['args']);
+
+}, 0);
