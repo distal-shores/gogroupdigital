c61a0ff75863f6cc1431224fb9b0c7a5f1676235
Clayton Green <claytron.g@gmail.com>
2019-12-05 22:55:30 -0800

    Show contact form if #contact is in the URL

--- wp-content/themes/Theme/js/src/modules/contact-form.js
+++ wp-content/themes/Theme/js/src/modules/contact-form.js
@@ -1,21 +1,36 @@
-jQuery(window).on('load', function() {
+jQuery(window).on('load', function () {
 	'use strict';
 
-	jQuery('.contact-button, .primary-nav__item--contact, .footer-nav__item--contact').click(function() {
+	const hideContactForm = function() {
+		jQuery('.contact-form').removeClass('active');
+		jQuery('html, body').removeClass('contact-active');
+		// remove #contact from the url
+		history.pushState('', document.title, window.location.pathname + window.location.search);
+	};
+
+	const showContactForm = function() {
 		jQuery('.contact-form').addClass('active');
 		jQuery('html, body').addClass('contact-active');
 		// If hitting escape key
-		jQuery(document).keyup(function(e) {
-			if (e.keyCode === 27) { 
-				jQuery('.contact-form').removeClass('active');
-				jQuery('html, body').removeClass('contact-active');
+		jQuery(document).keyup(function (e) {
+			if (e.keyCode === 27) {
+				hideContactForm();
 			}
 		});
+	};
+
+	// Check if the url has `#contact` and show the contact form if it does
+	const currentUrlFragment = window.location.hash;
+	if (currentUrlFragment === '#contact') {
+		showContactForm();
+	}
+
+	jQuery('.contact-button, .primary-nav__item--contact, .footer-nav__item--contact').click(function () {
+		showContactForm();
 	});
 
 	// If hitting close
-	jQuery('.contact-form__form__close').click(function() {
-		jQuery('.contact-form').removeClass('active');
-		jQuery('html, body').removeClass('contact-active');
+	jQuery('.contact-form__form__close').click(function () {
+		hideContactForm();
 	});
 });
