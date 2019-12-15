ea6053647e5ee752a23b341da77fa9b16ef170aa
Clayton Green <claytron.g@gmail.com>
2019-12-14 22:18:28 -0800

    Remove original footnote elements after cloning and moving

--- wp-content/themes/Theme/js/footnote-append.js
+++ wp-content/themes/Theme/js/footnote-append.js
@@ -3,6 +3,7 @@
 		if ($('.easy-footnotes-wrapper').length > 0) {
 			$('.blog-post__footnotes').append('<h3>Sources</h3>');
 			$('.easy-footnotes-wrapper').clone().appendTo('.blog-post__footnotes');
+			$('.easy-footnotes-wrapper')[0].remove();
 		}
 	});
 })(jQuery);
