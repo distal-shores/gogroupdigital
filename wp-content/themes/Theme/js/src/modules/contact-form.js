9b68e205f50bec06807aa2598fcf3b35bcfb6c44
Clayton Green <claytron.g@gmail.com>
2020-01-26 23:26:43 -0800

    Fix grunt issues

--- wp-content/themes/Theme/js/src/modules/contact-form.js
+++ wp-content/themes/Theme/js/src/modules/contact-form.js
@@ -1,46 +1,46 @@
 jQuery(window).on('load', function () {
-	'use strict';
+  'use strict';
 
-	const hideContactForm = function () {
-		jQuery('.contact-form').removeClass('active');
-		jQuery('html, body').removeClass('contact-active');
-		// remove #contact from the url
-		history.pushState('', document.title, window.location.pathname + window.location.search);
-	};
+  var hideContactForm = function () {
+    jQuery('.contact-form').removeClass('active');
+    jQuery('html, body').removeClass('contact-active');
+    // remove #contact from the url
+    history.pushState('', document.title, window.location.pathname + window.location.search);
+  };
 
-	const showContactForm = function () {
-		jQuery('.contact-form').addClass('active');
-		jQuery('html, body').addClass('contact-active');
-		// If hitting escape key
-		jQuery(document).keyup(function (e) {
-			if (e.keyCode === 27) {
-				hideContactForm();
-			}
-		});
-	};
+  var showContactForm = function () {
+    jQuery('.contact-form').addClass('active');
+    jQuery('html, body').addClass('contact-active');
+    // If hitting escape key
+    jQuery(document).keyup(function (e) {
+      if (e.keyCode === 27) {
+        hideContactForm();
+      }
+    });
+  };
 
-	const hashMatchesContact = function () {
-		return window.location.hash === '#contact';
-	};
+  var hashMatchesContact = function () {
+    return window.location.hash === '#contact';
+  };
 
-	window.addEventListener('hashchange', function () {
-		if (hashMatchesContact()) {
-			showContactForm();
-		}
-	}, false);
+  window.addEventListener('hashchange', function () {
+    if (hashMatchesContact()) {
+      showContactForm();
+    }
+  }, false);
 
-	// If hitting close
-	jQuery('.contact-form__form__close').click(function () {
-		hideContactForm();
-	});
+  // If hitting close
+  jQuery('.contact-form__form__close').click(function () {
+    hideContactForm();
+  });
 
-	// Check if the url has `#contact` and show the contact form if it does
-	if (hashMatchesContact()) {
-		if (window.location.pathname.includes('thank-you')) {
-			hideContactForm();
-		} else {
-			showContactForm();
-		}
-	}
+  // Check if the url has `#contact` and show the contact form if it does
+  if (hashMatchesContact()) {
+    if (window.location.pathname.includes('thank-you')) {
+      hideContactForm();
+    } else {
+      showContactForm();
+    }
+  }
 
 });
--- wp-content/themes/Theme/js/src/modules/scroll_and_reveal.js
+++ wp-content/themes/Theme/js/src/modules/scroll_and_reveal.js
@@ -1,12 +1,12 @@
-jQuery(window).on('load', function() {
+jQuery(window).on('load', function () {
 	'use strict';
 
 	// Scroll and reveal CTA on home
-	window.sr = ScrollReveal();
-	sr.reveal('.cta', {duration: 1000, scale: 1, viewFactor: 0.2});
-	sr.reveal('.blog-tile', {duration: 1000, scale: 1, viewFactor: 0.2});
-	sr.reveal('.data-box', {duration: 1000, scale: 1, viewFactor: 0.2});
-	sr.reveal('.location-tile', {duration: 1000, scale: 1, viewFactor: 0.2});
-	sr.reveal('.member-tile', {duration: 1000, scale: 1, viewFactor: 0.2});
-	sr.reveal('.hero__content *', {duration: 1000, scale: 1});
+	window.sr = new ScrollReveal();
+	sr.reveal('.cta', { duration: 1000, scale: 1, viewFactor: 0.2 });
+	sr.reveal('.blog-tile', { duration: 1000, scale: 1, viewFactor: 0.2 });
+	sr.reveal('.data-box', { duration: 1000, scale: 1, viewFactor: 0.2 });
+	sr.reveal('.location-tile', { duration: 1000, scale: 1, viewFactor: 0.2 });
+	sr.reveal('.member-tile', { duration: 1000, scale: 1, viewFactor: 0.2 });
+	sr.reveal('.hero__content *', { duration: 1000, scale: 1 });
 });
